bbef0fb05c9125ba0069b2e10afaf9c6b1f2f4ae
Joseph <j_seremba@live.com>
2017-04-06 16:59:11 +0300

    Initial commit

---
+++ covert.js
@@ -0,0 +1,20 @@
+/*
+* Write a function that converts user entered date formatted as M/D/YYYY 
+* to a format required by an API (YYYYMMDD). The parameter "userDate" and 
+* the return value are strings.
+* For example, it should convert user entered date "12/31/2016" to "20161231" suitable for the API.
+*/
+
+function ConvertsDate(userDate) { 
+  userDate = new Date(userDate);
+  y = userDate.getFullYear().toString();
+  m = (userDate.getMonth() + 1).toString();
+  d = userDate.getDate().toString();
+  if (m.length == 1) m = '0' + m;
+  if (d.length == 1) d = '0' + d;
+  return y + m + d;
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+ConvertsDate(ConvertsDate()); 
